9e87fa6c52eb20caca6a1a7950e38a279848f745
Mohamed-hesham100 <apohamed1235@gmail.com>
2025-06-10 22:50:58 +0300

    ...

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-henna-chi.vercel.app",
     credentials: true,
   })
 );
